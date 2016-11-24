9cf0afdf940159bb3d6f2bebf95a24b8c732a977
atsushi130 <atsushi_main@outlook.jp>
2016-11-25 00:50:25 +0900

    hello, world

---
+++ app/ts/app.ts
@@ -0,0 +1,24 @@
+/// <reference path='../../typings/index.d.ts' />
+
+import { bootstrap }                 from 'angular2/platform/browser';
+import { Component, enableProdMode } from 'angular2/core';
+import { CORE_DIRECTIVES }           from 'angular2/common';
+
+enableProdMode();
+
+@Component({
+    selector: `wellcome`,
+    template: `<h1>Wellcome to Atsushi Lab !!</h1>`
+})
+class Wellcome {
+}
+
+@Component({
+    selector: 'my-app',
+    template: `<wellcome></wellcome>`,
+    directives: [CORE_DIRECTIVES, Wellcome]
+})
+class MyApp {
+}
+
+bootstrap(MyApp);
