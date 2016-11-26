47e50f97b9b6bf06bef29192a18f918b2ba46411
atsushi130 <atsushi_main@outlook.jp>
2016-11-26 19:32:42 +0900

    load navigation bar

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -7,21 +7,27 @@ import { bootstrap }                 from 'angular2/platform/browser';
 import { Component, enableProdMode } from 'angular2/core';
 import { CORE_DIRECTIVES }           from 'angular2/common';
 
+import NavigationBar from './navigation/navigationBar';
+
 enableProdMode();
 
 @Component({
     selector: `wellcome`,
-    template: `<h1>Wellcome to Atsushi Lab !!</h1>`
+    template: `<h1>{{message}}</h1>`
 })
 class Wellcome {
+    private message = 'Wellcome to Atsusi Lab !!';
 }
 
 @Component({
-    selector: 'my-app',
-    template: `<wellcome></wellcome>`,
-    directives: [CORE_DIRECTIVES, Wellcome]
+    selector: 'app',
+    template: `
+    <navigation-bar></navigation-bar>
+    <wellcome></wellcome>
+    `,
+    directives: [CORE_DIRECTIVES, Wellcome, NavigationBar]
 })
-class MyApp {
+class App {
 }
 
-bootstrap(MyApp);
+bootstrap(App);
