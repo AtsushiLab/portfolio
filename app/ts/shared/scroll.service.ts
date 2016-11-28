25eb96b1215beb5e839c7ce2c42ccfb31985c914
atsushi130 <atsushi_main@outlook.jp>
2016-11-29 00:20:31 +0900

    made scroll event handler service

--- app/ts/shared/scroll.service.ts
+++ app/ts/shared/scroll.service.ts
@@ -1,31 +1,32 @@
 import { Injectable, HostListener, ElementRef } from '@angular/core';
+import { Color }                                from './color';
 
 @Injectable()
 export class ScrollService {
 
 //    type UpdateStateHandler: () => string;
     private element: ElementRef;
-    public updateStateHandler: () => string;
+    public updateStateHandler: (str: string) => void;
 
-    constructor(elementRef: ElementRef, updateStateHandler: () => string) {
+    constructor(elementRef: ElementRef, updateStateHandler: (str: string) => void)  {
         this.element            = elementRef;
         this.updateStateHandler = updateStateHandler;
+
+        window.addEventListener('scroll', (event) => {
+
+            const scrollTop = this.element.nativeElement.scrollTop;
+
+            if (scrollTop < 300) {
+                this.updateStateHandler(Color.Dark);
+            }
+
+            if (scrollTop >= 300 && scrollTop < 500) {
+                this.updateStateHandler(Color.Navy);
+            }
+
+            if (scrollTop >= 500 && scrollTop < 700) {
+                this.updateStateHandler(Color.Green);
+            }
+        });
     }
-
-    @HostListener('document:scroll', ['$event'])
-    private onScroll(event: any) {
-    
-        const scrollTop = this.element.nativeElement.scrollTop;
-
-        if (scrollTop < 300) {
-        }
-
-        if (scrollTop >= 300 && scrollTop < 500) {
-        }
-
-        if (scrollTop >= 500 && scrollTop < 700) {
-        }
-
-        this.updateStateHandler();
-    }   
 }
