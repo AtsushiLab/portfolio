6f20e4fffd111d3b13abbb44949f0b99e1634e50
atsushi130 <atsushi_main@outlook.jp>
2016-11-29 16:00:22 +0900

    refactoring

--- app/ts/shared/scroll.service.ts
+++ app/ts/shared/scroll.service.ts
@@ -8,26 +8,31 @@ export class ScrollService {
 
     private element: ElementRef;
     public changeColorHandler: ChangeColorHandler;
+    static readonly CountOfColor: number = 3;
 
     constructor(elementRef: ElementRef, changeColorHandler: ChangeColorHandler)  {
         this.element            = elementRef;
         this.changeColorHandler = changeColorHandler;
 
-        window.addEventListener('scroll', (event) => {
-
-            const scrollTop = this.element.nativeElement.scrollTop;
-
-            if (scrollTop < 300) {
-                this.changeColorHandler(Color.Dark);
-            }
-
-            if (scrollTop >= 300 && scrollTop < 500) {
-                this.changeColorHandler(Color.Navy);
-            }
-
-            if (scrollTop >= 500 && scrollTop < 700) {
-                this.changeColorHandler(Color.Green);
-            }
-        });
+        window.addEventListener('scroll', this.onScroll);
     }
+
+    private onScroll() {
+
+        const height    = window.parent.screen.height
+        const scrollTop = this.element.nativeElement.ownerDocument.activeElement.scrollTop;
+
+        if (scrollTop < height / ScrollService.CountOfColor) {
+            this.changeColorHandler(Color.Dark);
+        }
+
+        if (scrollTop >= height / ScrollService.CountOfColor && scrollTop < (height / ScrollService.CountOfColor) * 2) {
+            this.changeColorHandler(Color.Navy);
+        }
+
+        if (scrollTop >= (height / ScrollService.CountOfColor) * 2 && scrollTop < height) {
+            this.changeColorHandler(Color.Green);
+        }
+    }
+
 }
